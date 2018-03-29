3ab06a8538c366d78c20854da25900cbb1dd7f3b
Matthias Arndt <marndt@asmsoftware.de>
2018-03-29 11:52:35 +0200

    - implement basic input and timing loop

--- src/startuip.c
+++ src/startuip.c
@@ -11,6 +11,16 @@
 #include <osbind.h>
 
 #define _200HZ_TICK (*((volatile uint32_t *)0x4ba))
+#define _5s (200U * 5U)
+
+typedef enum
+{
+    WAIT_FOR_INITIAL_INPUT,
+    RUN_UIP_ONCE,
+    RUN_UIP_LOOP,
+    REPEAT_UIP,
+    TERMINATE
+} RunState;
 
 static uint32_t timer_200_hz;
 
@@ -49,13 +59,109 @@ int main(void)
 {
     uint32_t tmr = Timer();
 
+    RunState st = WAIT_FOR_INITIAL_INPUT;
+
     Print("\r\n\033p   Start utility for uIP-Tool   \033q\r\n");
     Print("(c) 2018 by Matthias Arndt <marndt@asmsoftware.de>\r\n");
+    Print("[u] activate uIP-tool\r\n[r] activate uIP-tool with repetition\r\n[q] immedietaly quit\r\n");
 
-    while((Timer() - tmr) < 1000)
+    flush_kbd();
+
+    do
     {
-        /* wait 5s */
+        switch(st)
+        {
+
+            case RUN_UIP_ONCE:
+            case RUN_UIP_LOOP:
+
+                Print("*** uIP-tool running ***\r\n");
+
+                if(st == RUN_UIP_LOOP)
+                {
+                   Print("uIP-tool will restart, press [q] to terminate\r\n");
+                   tmr = Timer();
+                   flush_kbd();
+                   st = REPEAT_UIP;
+                }
+                else
+                {
+                    st = TERMINATE;
+                }
+
+            break;
+
+            case REPEAT_UIP:
+                if(Timer() - tmr > _5s)
+                {
+                    st = RUN_UIP_LOOP;
+                }
+                else if(Bconstat(2) != 0)
+                {
+                    uint32_t keycode = Bconin(2);
+                    uint8_t ascii = (keycode & 0x000000FFUL);
+
+                    if((ascii == 'q')||(ascii == 'Q'))
+                    {
+                        st = TERMINATE;
+                    }
+
+                    flush_kbd();
+                }
+                else
+                {
+                    /* wait for timeout */
+                }
+
+            break;
+
+            case WAIT_FOR_INITIAL_INPUT:
+                st = WAIT_FOR_INITIAL_INPUT;
+
+                if(Timer() - tmr > _5s)
+                {
+                    st = TERMINATE;
+                }
+                else if(Bconstat(2) != 0)
+                {
+                    uint32_t keycode = Bconin(2);
+                    uint8_t ascii = (keycode & 0x000000FFUL);
+
+                    if((ascii == 'q')||(ascii == 'Q'))
+                    {
+                        st = TERMINATE;
+                    }
+                    else if((ascii == 'u')||(ascii == 'U'))
+                    {
+                        st = RUN_UIP_ONCE;
+                    }
+                    else if((ascii == 'r')||(ascii == 'R'))
+                    {
+                        st = RUN_UIP_LOOP;
+                    }
+                    else
+                    {
+                        /* key not handled */
+                    }
+
+                    flush_kbd();
+                }
+                else
+                {
+                    /* wait for timeout */
+                }
+
+
+            break;
+
+            default:
+                tmr = Timer();
+                st = WAIT_FOR_INITIAL_INPUT;
+            break;
+        }
+
     }
+    while(st != TERMINATE);
 
     flush_kbd();
 	return 0;
